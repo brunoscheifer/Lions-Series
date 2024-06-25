95d94dfd017ac61d6a9a7d984cc124d6bfc24777
brunoscheifer <brunoscheifer11@gmail.com>
2024-06-25 17:35:23 -0300

    LionsSeries
    LionsSeries

--- LionsSeries.js
+++ LionsSeries.js
@@ -7,7 +7,7 @@ const rl = readline.createInterface ({
 
 let series = []
 
-exibitMenu()
+exibirMenu()
 function exibirMenu() {
     console.log(`
     Menu:
@@ -28,7 +28,7 @@ rl.question('Escolha uma opção: ', (opcao) => {
             editarSerie()
             break
         case '3':
-            listarserie()
+            listarSerie()
             break
         case '4':
             removerSerie()
@@ -37,11 +37,120 @@ rl.question('Escolha uma opção: ', (opcao) => {
             marcarConcluido()
             break
         case '6':
-            rl.close
+            rl.close()
+            break
         default:
             console.log('Numero invalido, tente denovo!')
             exibirMenu()
             break
+        }
+    })
+}
+
+function adicionarSerie() {
+    rl.question('Qual o nome da série?: ', (nome) => {
+        rl.question('Qual o ano de lançamento da série: ', (ano) => {
+            rl.question('Qual o genero da série: ', (genero) => {
+                rl.question('Qual o(a/as) ator/atriz principal da série: ', (principal) => {
+                    rl.question('Qual o diretor(a) da série: ', (diretor) => {
+
+                        series.push({nome: nome, ano: parseInt(ano), genero: genero, principal: principal, diretor: diretor, concluido: false})
+                        console.log('Série adicionada com sucesso!')
+                        exibirMenu()
+                    })
+                })
+            })
+        })
+    })
+}
+
+function editarSerie() {
+    if (series.length == 0){
+		console.log("Não há séries adicionadas")
+	} else if (series.length == []) {
+        listarSerie()
+            rl.question('Digite a série que voçê quer editar: ', (numero) => {
+                if(numero > 0 && numero <= series.length) {
+                    rl.question('Digite o novo nome: ', (nome) => {
+                        rl.question('Digite o novo ano: ', (ano) => {
+                            rl.question('Digite o novo genero: ', (genero) => {
+                                rl.question('Digite o novo protagonista: ', (principal) => {
+                                    rl.question('Digite o novo diretor: ', (diretor) => {
+                                        series[numero - 1] = {
+                                            nome,
+                                            ano,
+                                            genero,
+                                            principal,
+                                            diretor,
+                                        }
+                                        console.log('Editado com sucesso')
+                                        exibirMenu()
+                                    })
+                                })
+                            }) 
+                        })
+                    })
+                } else {
+                    console.log('\nNúmero inválido, tente novamente.')
+					exibirMenu()
+                }
+            })
+        }
     }
-})
+
+
+
+function listarSerie() {
+	if (series.length === 0) {
+		console.log('Nenhuma série adicionado ainda!')
+		exibirMenu()
+	} else {
+		console.log('Lista de séries:')
+		series.forEach((series, index) => {
+            console.log(`
+            ${index + 1}. Nome: ${series.nome}
+            Ano: ${series.ano}
+            Genero: ${series.genero}
+            Protagonista: ${series.principal}
+            Diretor: ${series.diretor}
+            Status: ${series.concluido}
+            `)
+        })
+        exibirMenu()
+    }
+}
+
+function removerSerie() {
+    if (series.length == 0) {
+        console.log('Nenhuma série adicionada ainda!')
+        exibirMenu()
+    } else {
+        listarSerie()
+		rl.question('Qual série deseja remover: ', (numero) => {
+			if (numero > 0 && numero <= jogos.length) {
+				series.splice(numero - 1, 1)
+				console.log('Série removida!')
+				exibirMenu()
+			} else {
+				console.log('Numero invalido, tente denovo')
+				exibirMenu()
+			}
+		})
+	}
+}
+
+function marcarConcluido() {
+	if (series.length === 0) {
+		console.log('Nenhuma série adicionada ainda!')
+		exibirMenu()
+	} else {
+		listarSerie()
+		rl.question('Qual série voçê quer marcar como concluida?: ', (numero) => {
+			if (numero > 0 && numero <= lembretes.length) {
+				series[numero-1].concluido = true
+			}
+			console.log("Marcado com sucesso")
+			exibirMenu()
+		}
+	)}
 }
